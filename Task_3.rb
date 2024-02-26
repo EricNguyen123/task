f4258d35425db02d8ca95b37c8d1b43b8c7569d1
Your Name <huyhuhyhoa@gmail.com>
2024-02-26 20:01:27 +0700

    fix task 3

--- Task_3.rb
+++ Task_3.rb
@@ -6,17 +6,20 @@ def print_calendar(year)
   days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
   days_in_month[1] = 29 if Date.leap?(year)
 
+  first_day = Date.new(year, 1, 1)
+  first_wday = first_day.wday
+
+  offset = first_wday
+
   (1..12).each do |month|
-    date = Date.new(year, month, 1)
-    wday = date.wday
     puts "    #{Date::MONTHNAMES[month]} #{year}"
     puts 'Su Mo Tu We Th Fr Sa'
-    print '   ' * wday
+    print '   ' * offset
     (1..days_in_month[month - 1]).each do |day|
       printf('%2d ', day)
-      puts if wday == 6 || day == days_in_month[month - 1]
-      wday = (wday + 1) % 7
+      puts if (offset + day) % 7 == 0 || day == days_in_month[month - 1]
     end
+    offset = (offset + days_in_month[month - 1]) % 7
     puts
   end
 end
